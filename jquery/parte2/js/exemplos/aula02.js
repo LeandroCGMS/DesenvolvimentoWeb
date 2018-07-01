7cbcc19c0358bb17894c17489ca707b8906a80f9
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-07-01 19:37:08 -0400

    22 finalizada

--- jquery/parte2/js/exemplos/aula02.js
+++ jquery/parte2/js/exemplos/aula02.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-  $("#filmes tr td").addClass("impar");
+  //$("#filmes tr:odd").addClass("impar");
+  $("#filmes tr:nth-child(4n+1)").addClass("impar");
+  $("#filmes tr:nth-child(4n)").addClass("impar");
 });
