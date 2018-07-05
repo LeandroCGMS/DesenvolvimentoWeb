eff3f69809907fe8f9f868cc4f9ea0a0f918c45d
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-07-04 20:59:20 -0400

    Aula 09 - parte 02

---
+++ jquery/parte2/js/exemplos/aula09.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  //SELECIONAR TODOS OS CHECKBOX
+
+  $("textarea").bind("input keyup paste", function(){
+    var maximo = 100;
+    var disponivel = maximo - $(this).val().length;
+    if(disponivel < 0){
+      var texto = $(this).val().substr(0,maximo);
+      $(this).val(texto);
+      disponivel = 0;
+    }
+    $(".contagem").text(disponivel);
+  });
+
+});
