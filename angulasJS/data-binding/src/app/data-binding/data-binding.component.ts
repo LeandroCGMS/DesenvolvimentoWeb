b18df0b17ab3ca55f54bde80cf57efb14c437ba9
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-12-20 10:26:34 -0300

    Curso Angular #09: Property Binding e Interpolação

---
+++ angulasJS/data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+  url = 'http://loiane.com';
+  cursoAngular = true;
+  urlImagem = 'https://picsum.photos/200/300/?random';
+  getValor() {
+    return 1;
+  }
+  getCurtirCurso() {
+    return true;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
