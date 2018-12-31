d4be8629069cbe81ce617efb6dae3c6c4d24e76c
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-31 14:20:53 -0300

    Curso Angular #32: ng-content

--- AngularCLI/angular-cli-libs-externas/src/app/app.module.ts
+++ AngularCLI/angular-cli-libs-externas/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-content.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemploNgContentComponent
   ],
   imports: [
     BrowserModule,
