1810499c8461eeed03a2fc72bda8af904f1638fb
Leandro Santos de Carvalho <leandrocgms2015@gmail.com>
2019-01-14 19:04:22 -0300

    Curso Angular #46: Pipes: Criando um Pipe "Puro"

--- AngularCLI/proj6-pipes/src/app/app.module.ts
+++ AngularCLI/proj6-pipes/src/app/app.module.ts
@@ -2,23 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 registerLocaleData(localePt, 'pt');
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
-import { SettingsService } from './settings.service'
+import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     /*
